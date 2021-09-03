4e814e2a8e76af2bc2689b3e9a7f8256915baf06
Bruno Ramos <bruno.ramos@in8.com.br>
2021-09-03 11:06:13 -0300

    Feat: cost center #7q2kwr

---
+++ app/Http/Controllers/CostCenterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreCostCenterRequest;
+use App\Http\Requests\PutCostCenterRequest;
+use App\Services\CostCenterService as CostCenterService;
+
+class CostCenterController extends Controller
+{
+
+    private $costCenterService;
+
+    public function __construct(CostCenterService $costCenterService)
+    {
+        $this->costCenterService = $costCenterService;
+    }
+
+    public function index()
+    {
+        return $this->costCenterService->getAllCostCenter();
+    }
+
+    public function show($id)
+    {
+        return $this->costCenterService->getCostCenter($id);
+    }
+
+    public function store(StoreCostCenterRequest $request)
+    {
+        $costCenter = $this->costCenterService->postCostCenter($request->all());
+        return response($costCenter, 201);
+    }
+
+    public function update(PutCostCenterRequest $request, $id)
+    {
+        return $this->costCenterService->putCostCenter($id, $request->all());
+    }
+
+    public function destroy($id)
+    {
+        $costCenter = $this->costCenterService->deleteCostCenter($id);
+        return response('');
+    }
+}
