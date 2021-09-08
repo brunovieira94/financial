e8134051d96179939c0ea07a4e53992d225a1c50
Alisson de S. Santos <alisson.souza@in8.com.br>
2021-09-08 09:54:55 -0300

    feat: added relationship model

--- app/Models/City.php
+++ app/Models/City.php
@@ -3,10 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\State;
 
 class City extends Model
 {
     use SoftDeletes;
     protected $table='cities';
     protected $fillable = ['title', 'states_id'];
+    protected $hidden = ['states_id'];
+
+    public function state(){
+        return $this->belongsTo(State::class, 'states_id', 'id');
+    }
 }
