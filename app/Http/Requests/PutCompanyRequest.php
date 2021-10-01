b5f5b099f56548f753c98824e5d79502232f619b
Bruno Ramos <bruno.ramos@in8.com.br>
2021-10-01 14:36:22 -0300

    refactor:bank-accounts

--- app/Http/Requests/PutCompanyRequest.php
+++ app/Http/Requests/PutCompanyRequest.php
@@ -23,10 +23,13 @@ class PutCompanyRequest extends FormRequest
             'number' => 'max:250',
             'complement' => 'max:150',
             'district' => 'max:150',
-            'bank_accounts.*.agency_number' => 'required_without_all:agency_check_number,account_number,account_check_number,bank_id,bank_accounts.*.id|integer',
-            'bank_accounts.*.account_number' => 'integer|required_without_all:agency_number,agency_check_number,account_check_number,bank_id,bank_accounts.*.id',
-            'bank_accounts.*.account_check_number' => 'integer|required_without_all:agency_number,agency_check_number,account_number,bank_id,bank_accounts.*.id',
-            'bank_accounts.*.bank_id' => 'integer|required_without_all:agency_number,agency_check_number,account_number,account_check_number,bank_accounts.*.id',
+            'bank_accounts.*.agency_number' => 'required_without_all:bank_accounts.*.pix_key|numeric',
+            'bank_accounts.*.agency_check_number' => 'integer|required_without_all:bank_accounts.*.pix_key',
+            'bank_accounts.*.account_number' => 'integer|required_without_all:bank_accounts.*.pix_key',
+            'bank_accounts.*.account_check_number' => 'integer|required_without_all:bank_accounts.*.pix_key',
+            'bank_accounts.*.bank_id' => 'integer|required_without_all:bank_accounts.*.pix_key',
+            'bank_accounts.*.pix_key' => 'string|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id',
+            'bank_accounts.*.account_type' => 'integer|required_without_all:bank_accounts.*.pix_key|min:0|max:2',
             'managers' => 'array',
         ];
     }
--- app/Http/Requests/StoreBankAccountRequest.php
+++ app/Http/Requests/StoreBankAccountRequest.php
@@ -21,7 +21,7 @@ class StoreBankAccountRequest extends FormRequest
             'account_number' => 'integer|required_without_all:pix_key',
             'account_check_number' => 'integer|required_without_all:pix_key',
             'bank_id' => 'integer|required_without_all:pix_key',
-            'pix_key' => 'string|required_without_all:agency_number,agency_check_number,account_number,account_check_number,account_type',
+            'pix_key' => 'string|required_without_all:agency_number,agency_check_number,account_number,account_check_number,account_type,bank_id',
             'account_type' => 'integer|required_without_all:pix_key|min:0|max:2',
        ];
     }
