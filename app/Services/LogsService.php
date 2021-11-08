d65011fcddd6c2dd947d4501a0fbfb1dfbe64626
Bruno Ramos <bruno.ramos@in8.com.br>
2021-11-08 19:57:15 -0300

    refactor: logs-remove-pagination-from-getLogs

--- app/Services/LogsService.php
+++ app/Services/LogsService.php
@@ -16,13 +16,10 @@ class LogsService
 
     public function getLogs($log_name, $subject_id, $requestInfo)
     {
-        $orderBy = $requestInfo['orderBy'] ?? Utils::defaultOrderBy;
-        $order = $requestInfo['order'] ?? Utils::defaultOrder;
-        $perPage = $requestInfo['perPage'] ?? Utils::defaultPerPage;
         return Activity::where([
             ['log_name', $log_name],
             ['subject_id', $subject_id]
-        ])->orderBy($orderBy, $order)->paginate($perPage);
+        ])->get();
     }
 
 }
