4f19cb1511159129fffe1bf9599efc5d58116df3
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-02-01 13:01:13 -0300

    feat: added validation laravel

--- app/Http/Requests/PutCityRequest.php
+++ app/Http/Requests/PutCityRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\DuplicateCity;
 
 class PutCityRequest extends FormRequest
 {
@@ -15,7 +16,7 @@ class PutCityRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => ['required_without:states_id', 'max:250', request()->input('states_id'), Request::instance()->id],
+            'title' => ['required_without:states_id', 'max:250', new DuplicateCity(request()->input('states_id'), Request::instance()->id)],
             'states_id' => 'required_without:title|integer',
         ];
     }
