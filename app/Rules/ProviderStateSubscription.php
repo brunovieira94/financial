cac78b6aa154182cdf58900808b3eb5484d573b9
Bruno Ramos <bruno.ramos@in8.com.br>
2022-02-09 09:26:41 -0300

    fix: provider-requests-city-and-state-subscription-rules

--- app/Rules/ProviderStateSubscription.php
+++ app/Rules/ProviderStateSubscription.php
@@ -8,17 +8,19 @@ class ProviderStateSubscription implements Rule
 {
     private $international;
     private $subscription;
+    private $type;
 
-    public function __construct($international = null, $subscription = null){
+    public function __construct($international = null, $subscription = null, $type = null){
         $this->international = $international;
         $this->subscription = $subscription;
+        $this->type = $type;
     }
 
     public function passes($attribute, $value)
     {
         if(!$this->international)
         {
-            if(!$this->subscription && !$value){
+            if(!$this->subscription && !$value && $this->type == 'J'){
                 return false;
             }
         }
