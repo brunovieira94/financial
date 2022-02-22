828bf2af33699a237c7a5230c9419eb87d6a5e83
Bruno Ramos <bruno.ramos@in8.com.br>
2022-02-22 10:24:49 -0300

    feat: changes in AllDuePaymentRequestExport

--- app/Exports/AllDuePaymentRequestExport.php
+++ app/Exports/AllDuePaymentRequestExport.php
@@ -4,14 +4,44 @@ namespace App\Exports;
 
 use App\Models\PaymentRequest;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class AllDuePaymentRequestExport implements FromCollection
+class AllDuePaymentRequestExport implements FromCollection, ShouldAutoSize, WithMapping, WithHeadings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+
+    use Exportable;
+
+    private $parent;
+
     public function collection()
     {
-        return PaymentRequest::all();
+        return PaymentRequest::get();
+    }
+
+    public function map($paymentRequest): array
+    {
+        // $this->parent = CostCenter::where('id', $costCenter->parent)->first();
+        // return [
+        //     $costCenter->title,
+        //     $costCenter->code,
+        //     $this->parent ? $this->parent->code : $costCenter->parent,
+        // ];
+
+        return [
+            $paymentRequest->title,
+            $paymentRequest->code,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Título',
+            'Código',
+            'Código do pai',
+        ];
     }
 }
