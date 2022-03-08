19d4f8106e3cac913e1e0d5196406a21f5212312
Anaximandro Andrade <anaxfive@hotmail.com>
2022-03-08 18:03:03 -0100

    Configured CI CD  added app name to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ env('APP_NAME') }}</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -401,7 +401,7 @@
 </head>
 
 <body class="antialiased">
-    <p><b>Ambiente:</b> {{ env('APP_ENV') }}</p>
+    <p><em>{{ env('APP_NAME') }}</em>|<b>Ambiente:</b> {{ env('APP_ENV') }}</p>
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
