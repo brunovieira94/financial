b706b31f094132fc844de1b33654a34e2b33c370
Anaximandro Andrade <anaxfive@hotmail.com>
2022-03-28 07:47:52 -0100

    enhancehaments in update-user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\User;
 use App\Models\CostCenter;
 use App\Models\Business;
@@ -23,13 +24,13 @@ class UserService
 
     public function getAllUser($requestInfo)
     {
-        $user = Utils::search($this->user,$requestInfo);
-        return Utils::pagination($user->with($this->with),$requestInfo);
+        $user = Utils::search($this->user, $requestInfo);
+        return Utils::pagination($user->with($this->with), $requestInfo);
     }
 
     public function getUser($id)
     {
-      return $this->user->with($this->with)->findOrFail($id);
+        return $this->user->with($this->with)->findOrFail($id);
     }
 
     public function postUser($userInfo)
@@ -41,14 +42,13 @@ class UserService
         self::syncBusiness($user, $userInfo);
         self::syncCostCenter($user, $userInfo);
         return $this->user->with($this->with)->findOrFail($user->id);
-
     }
 
     public function putUser($id, $userInfo)
     {
         $user = $this->user->findOrFail($id);
 
-        if(array_key_exists('password', $userInfo)){
+        if (array_key_exists('password', $userInfo)) {
             $userInfo['password'] = Hash::make($userInfo['password']);
         }
         $user->fill($userInfo)->save();
@@ -61,41 +61,46 @@ class UserService
 
     public function deleteUser($id)
     {
-      $this->user->findOrFail($id)->delete();
-      return true;
+        $this->user->findOrFail($id)->delete();
+        return true;
     }
 
     public function updateMyUser($userInfo)
     {
-        if(!Hash::check($userInfo['password'], auth()->user()->password))
-        {
-            return Response()->json([
-                'erro' => 'A senha informada é inválida',
-            ]);
-        }
 
-        if(array_key_exists('new-password', $userInfo))
-        {
-            $userInfo['password'] = Hash::make($userInfo['new-password']);
-        } else
-        {
+        $user = User::findOrFail(auth()->user()->id);
+
+        if (array_key_exists('password', $userInfo) && array_key_exists('new-password', $userInfo)) {
+            if (!Hash::check($userInfo['password'], auth()->user()->password)) {
+                return Response()->json([
+                    'erro' => 'A senha informada é inválida',
+                ], 422);
+            }
+
+            if (array_key_exists('new-password', $userInfo)) {
+                $userInfo['password'] = Hash::make($userInfo['new-password']);
+            } else {
+                unset($userInfo['password']);
+            }
+        } else {
             unset($userInfo['password']);
         }
 
-        $user = User::findOrFail(auth()->user()->id);
         $user->fill($userInfo)->save();
 
         return $this->user->with($this->with)->findOrFail(auth()->user()->id);
     }
 
-    public function syncCostCenter($user, $userInfo){
-        if(array_key_exists('cost_centers', $userInfo)){
+    public function syncCostCenter($user, $userInfo)
+    {
+        if (array_key_exists('cost_centers', $userInfo)) {
             $user->cost_center()->sync($userInfo['cost_centers']);
         }
     }
 
-    public function syncBusiness($user, $userInfo){
-        if(array_key_exists('business', $userInfo)){
+    public function syncBusiness($user, $userInfo)
+    {
+        if (array_key_exists('business', $userInfo)) {
             $user->business()->sync($userInfo['business']);
         }
     }
