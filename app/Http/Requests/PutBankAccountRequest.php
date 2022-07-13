02694d9cb5a7302fb3447f888497888617ae2533
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-07-13 08:48:52 -0300

    fix problem request

--- app/Http/Requests/PutBankAccountRequest.php
+++ app/Http/Requests/PutBankAccountRequest.php
@@ -15,13 +15,13 @@ class PutBankAccountRequest extends FormRequest
     {
         return [
             'agency_number' => 'required_without_all:pix_key|numeric',
-            //'agency_check_number' => 'required_without_all:bank_accounts.*.pix_key',
-            'account_number' => 'numeric|required_without_all:bank_accounts.*.pix_key',
-            //'account_check_number' => 'required_without_all:bank_accounts.*.pix_key',
-            'bank_id' => 'integer|required_without_all:bank_accounts.*.pix_key|exists:banks,id',
-            'pix_key' => 'string|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id',
-            'pix_key_type' => 'integer|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id|min:0|max:4',
-            'account_type' => 'integer|required_without_all:bank_accounts.*.pix_key|min:0|max:2',
+            //'agency_check_number' => 'required_without_all:pix_key',
+            'account_number' => 'numeric|required_without_all:pix_key',
+            //'account_check_number' => 'required_without_all:pix_key',
+            'bank_id' => 'integer|required_without_all:pix_key|exists:banks,id',
+            'pix_key' => 'string|required_without_all:agency_number,agency_check_number,account_number,account_check_number,account_type,bank_id',
+            'pix_key_type' => 'integer|required_without_all:agency_number,agency_check_number,account_number,account_check_number,account_type,bank_id|min:0|max:4',
+            'account_type' => 'integer|required_without_all:pix_key|min:0|max:2',
        ];
     }
 }
