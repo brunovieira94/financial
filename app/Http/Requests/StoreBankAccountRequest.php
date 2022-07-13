fc35ef1d3accf7a15cf0e84764ac8c8fafe059b5
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-07-13 08:44:16 -0300

    fix: solve problem validation

--- app/Http/Requests/StoreBankAccountRequest.php
+++ app/Http/Requests/StoreBankAccountRequest.php
@@ -16,14 +16,14 @@ class StoreBankAccountRequest extends FormRequest
     public function rules()
     {
         return [
-            'bank_accounts.agency_number' => 'required_without_all:bank_accounts.*.pix_key|numeric',
-            //'bank_accounts.agency_check_number' => 'required_without_all:bank_accounts.*.pix_key',
-            'bank_accounts.account_number' => 'numeric|required_without_all:bank_accounts.*.pix_key',
-            //'bank_accounts.account_check_number' => 'required_without_all:bank_accounts.*.pix_key',
-            'bank_accounts.bank_id' => 'integer|required_without_all:bank_accounts.*.pix_key|exists:banks,id',
-            'bank_accounts.pix_key' => 'string|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id',
-            'bank_accounts.pix_key_type' => 'integer|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id|min:0|max:4',
-            'bank_accounts.account_type' => 'integer|required_without_all:bank_accounts.*.pix_key|min:0|max:2',
+            'agency_number' => 'required_without_all:bank_accounts.*.pix_key|numeric',
+            //'agency_check_number' => 'required_without_all:bank_accounts.*.pix_key',
+            'account_number' => 'numeric|required_without_all:bank_accounts.*.pix_key',
+            //'account_check_number' => 'required_without_all:bank_accounts.*.pix_key',
+            'bank_id' => 'integer|required_without_all:bank_accounts.*.pix_key|exists:banks,id',
+            'pix_key' => 'string|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id',
+            'pix_key_type' => 'integer|required_without_all:bank_accounts.*.agency_number,bank_accounts.*.agency_check_number,bank_accounts.*.account_number,bank_accounts.*.account_check_number,bank_accounts.*.account_type,bank_accounts.*.bank_id|min:0|max:4',
+            'account_type' => 'integer|required_without_all:bank_accounts.*.pix_key|min:0|max:2',
        ];
     }
 }
