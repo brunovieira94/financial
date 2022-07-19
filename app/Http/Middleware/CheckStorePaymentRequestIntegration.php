7cb24d4dcae9802e35b368f262f7a343ee4c7419
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-07-19 18:14:37 -0300

    fix: solve problem alter value integration

--- app/Http/Middleware/CheckStorePaymentRequestIntegration.php
+++ app/Http/Middleware/CheckStorePaymentRequestIntegration.php
@@ -25,16 +25,15 @@ class CheckStorePaymentRequestIntegration
             $amountReceived = 0;
             foreach ($paymentRequestInfo['installment_purchase_order'] as $instalmentPurchaseOrder) {
                 $amountReceived += $instalmentPurchaseOrder['amount_received'];
-
                 $installmentPurchase = PurchaseOrderHasInstallments::FindOrFail($instalmentPurchaseOrder['installment']);
                 if ($request->route('id') != null) {
-                    if(PaymentRequestHasPurchaseOrderInstallments::where('payment_request_id', $request->route('id'))->where('purchase_order_has_installments_id')->exists()){
-                        $paymentRequestHasPurchaseOrderInstallments = PaymentRequestHasPurchaseOrderInstallments::where('payment_request_id', $request->route('id'))->where('purchase_order_has_installments_id')->first();
+                    if(PaymentRequestHasPurchaseOrderInstallments::where('payment_request_id', $request->route('id'))->where('purchase_order_has_installments_id', $instalmentPurchaseOrder['installment'])->exists()){
+                        $paymentRequestHasPurchaseOrderInstallments = PaymentRequestHasPurchaseOrderInstallments::where('payment_request_id', $request->route('id'))->where('purchase_order_has_installments_id', $instalmentPurchaseOrder['installment'])->first();
                         $installmentPurchase->amount_paid -= $paymentRequestHasPurchaseOrderInstallments->amount_received;
                     }
                 }
                 $installmentPurchase->amount_paid += $instalmentPurchaseOrder['amount_received'];
-                if ($installmentPurchase->amount_paid > $installmentPurchase->portion_amount) {
+                if ($installmentPurchase->amount_paid > ($installmentPurchase->portion_amount - $installmentPurchase->money_discount)) {
                     return response()->json([
                         'error' => 'A soma do valor recebido para a parcela: ' . $installmentPurchase->id . ' é maior do que necessário para o pagamento completo da mesma.'
                     ], 422);
