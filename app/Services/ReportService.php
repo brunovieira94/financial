3d206da0b69f9bae48bd0289ac9bf279ee358d07
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-07-28 15:24:13 -0300

    fix: solve proble reports installmente payable

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -792,32 +792,7 @@ class ReportService
             }
         }
 
-        $query = Utils::pagination($query, $requestInfo);
-
-        foreach ($query as $paymentRequest) {
-            foreach ($paymentRequest->purchase_order as $purchaseOrder) {
-                foreach ($purchaseOrder->purchase_order_installments as $key => $installment) {
-                    $installment = [
-                        'id' => $installment->installment_purchase->id,
-                        'amount_received' => $installment->amount_received,
-                        'purchase_order_id' => $installment->installment_purchase->purchase_order_id,
-                        'parcel_number' => $installment->installment_purchase->parcel_number,
-                        'portion_amount' => $installment->installment_purchase->portion_amount,
-                        'due_date' => $installment->installment_purchase->due_date,
-                        'note' => $installment->installment_purchase->note,
-                        'percentage_discount' => $installment->installment_purchase->percentage_discount,
-                        'money_discount' => $installment->installment_purchase->money_discount,
-                        'invoice_received' => $installment->installment_purchase->invoice_received,
-                        'invoice_paid' => $installment->installment_purchase->invoice_paid,
-                        'payment_request_id' => $installment->installment_purchase->payment_request_id,
-                        'amount_paid' => $installment->installment_purchase->amount_paid,
-                    ];
-
-                    $purchaseOrder->purchase_order_installments[$key] = $installment;
-                }
-            }
-        }
-        return $query;
+        return Utils::pagination($query, $requestInfo);
     }
 
     public function getAllPaymentRequestFinished($requestInfo)
