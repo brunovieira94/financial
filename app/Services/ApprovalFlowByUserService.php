3d0060322663adc4c7fd7445c29cbf2ccbafc8b6
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-07-29 10:19:06 -0300

    CU-37h67eh: solve problem data reports

--- app/Services/ApprovalFlowByUserService.php
+++ app/Services/ApprovalFlowByUserService.php
@@ -6,6 +6,7 @@ use App\Models\AccountsPayableApprovalFlow;
 use App\Models\AccountsPayableApprovalFlowClean;
 use App\Models\ApprovalFlow;
 use App\Models\PaymentRequest;
+use App\Models\PaymentRequestClean;
 use App\Models\PaymentRequestHasInstallments;
 use Illuminate\Http\Request;
 use Config;
@@ -16,12 +17,16 @@ class ApprovalFlowByUserService
     private $accountsPayableApprovalFlow;
     private $approvalFlow;
     private $accountsPayableApprovalFlowClean;
+    private $paymentRequestClean;
 
-    public function __construct(AccountsPayableApprovalFlowClean $accountsPayableApprovalFlowClean, AccountsPayableApprovalFlow $accountsPayableApprovalFlow, ApprovalFlow $approvalFlow)
+    private $paymentRequestCleanWith = ['provider', 'cost_center', 'approval.approval_flow', 'installments', 'currency', 'cnab_payment_request.cnab_generated'];
+
+    public function __construct(PaymentRequestClean $paymentRequestClean, AccountsPayableApprovalFlowClean $accountsPayableApprovalFlowClean, AccountsPayableApprovalFlow $accountsPayableApprovalFlow, ApprovalFlow $approvalFlow)
     {
         $this->accountsPayableApprovalFlow = $accountsPayableApprovalFlow;
         $this->approvalFlow = $approvalFlow;
         $this->accountsPayableApprovalFlowClean = $accountsPayableApprovalFlowClean;
+        $this->paymentRequestClean = $paymentRequestClean;
     }
 
     public function getAllAccountsForApproval($requestInfo)
@@ -31,120 +36,17 @@ class ApprovalFlowByUserService
         if (!$approvalFlowUserOrder)
             return response([], 404);
 
-        $accountsPayableApprovalFlow = Utils::search($this->accountsPayableApprovalFlowClean, $requestInfo, ['order']);
+        $paymentRequest = Utils::search($this->paymentRequestClean, $requestInfo, ['order']);
 
-        $accountsPayableApprovalFlow->whereIn('order', $approvalFlowUserOrder->toArray())
-            ->whereIn('status', [0, 2])
-            ->whereRelation('payment_request', 'deleted_at', '=', null)
-            ->with(['payment_request.provider', 'payment_request.company', 'payment_request.cost_center', 'payment_request.currency', 'approval_flow']);
-
-        $accountsPayableApprovalFlow->whereHas('payment_request', function ($query) use ($requestInfo) {
-            if (array_key_exists('provider', $requestInfo)) {
-                $query->where('provider_id', $requestInfo['provider']);
-            }
-            if (array_key_exists('net_value', $requestInfo)) {
-                $query->where('net_value', $requestInfo['net_value']);
-            }
-            if (array_key_exists('company', $requestInfo)) {
-                $query->where('company_id', $requestInfo['company']);
-            }
-            if (array_key_exists('cost_center', $requestInfo)) {
-                $query->where('cost_center_id', $requestInfo['cost_center']);
-            }
-            if (array_key_exists('cpfcnpj', $requestInfo)) {
-                $query->whereHas('provider', function ($query) use ($requestInfo) {
-                    $query->where('cpf', $requestInfo['cpfcnpj'])->orWhere('cnpj', $requestInfo['cpfcnpj']);
-                });
-            }
-            if (array_key_exists('chart_of_accounts', $requestInfo)) {
-                $query->where('chart_of_account_id', $requestInfo['chart_of_accounts']);
-            }
-            if (array_key_exists('payment_request', $requestInfo)) {
-                $query->where('id', $requestInfo['payment_request']);
-            }
-            if (array_key_exists('user', $requestInfo)) {
-                $query->where('user_id', $requestInfo['user']);
-            }
-            if (array_key_exists('created_at', $requestInfo)) {
-                if (array_key_exists('from', $requestInfo['created_at'])) {
-                    $query->where('created_at', '>=', $requestInfo['created_at']['from']);
-                }
-                if (array_key_exists('to', $requestInfo['created_at'])) {
-                    $query->where('created_at', '<=', date("Y-m-d", strtotime("+1 days", strtotime($requestInfo['created_at']['to']))));
-                }
-                if (!array_key_exists('to', $requestInfo['created_at']) && !array_key_exists('from', $requestInfo['created_at'])) {
-                    $query->whereBetween('created_at', [now()->addMonths(-1), now()]);
-                }
-            }
-            if (array_key_exists('pay_date', $requestInfo)) {
-                if (array_key_exists('from', $requestInfo['pay_date'])) {
-                    $query->where('pay_date', '>=', $requestInfo['pay_date']['from']);
-                }
-                if (array_key_exists('to', $requestInfo['pay_date'])) {
-                    $query->where('pay_date', '<=', $requestInfo['pay_date']['to']);
-                }
-                if (!array_key_exists('to', $requestInfo['pay_date']) && !array_key_exists('from', $requestInfo['pay_date'])) {
-                    $query->whereBetween('pay_date', [now(), now()->addMonths(1)]);
-                }
-            }
-            if (array_key_exists('extension_date', $requestInfo)) {
-                if (array_key_exists('from', $requestInfo['extension_date'])) {
-                    $query->whereHas('installments', function ($installments) use ($requestInfo) {
-                        if (array_key_exists('from', $requestInfo['extension_date'])) {
-                            $installments->where('extension_date', '>=', $requestInfo['extension_date']['from']);
-                        }
-                        if (array_key_exists('to', $requestInfo['extension_date'])) {
-                            $installments->where('extension_date', '<=', $requestInfo['extension_date']['to']);
-                        }
-                        if (!array_key_exists('to', $requestInfo['extension_date']) && !array_key_exists('from', $requestInfo['extension_date'])) {
-                            $installments->whereBetween('extension_date', [now(), now()->addMonths(1)]);
-                        }
-                    });
-                }
-            }
-            if (array_key_exists('days_late', $requestInfo)) {
-                $query->whereHas('installments', function ($query) use ($requestInfo) {
-                    $query->where('status', '!=', Config::get('constants.status.paid out'))->orWhereNull('status')->whereDate("due_date", "<=", Carbon::now()->subDays($requestInfo['days_late']));
-                });
-            }
+        $paymentRequest->whereHas('approval', function ($query) use ($approvalFlowUserOrder) {
+            $query->whereIn('order', $approvalFlowUserOrder->toArray())
+                ->whereIn('status', [0, 2])
+                ->whereRelation('payment_request', 'deleted_at', '=', null)
+                ->with($this->paymentRequestCleanWith);
         });
-
-        if (array_key_exists('approval_order', $requestInfo)) {
-            $accountsPayableApprovalFlow->where('order', $requestInfo['approval_order']);
-        }
-
-        if (array_key_exists('status', $requestInfo)) {
-            $accountsPayableApprovalFlow->where('status', $requestInfo['status']);
-        }
-
-        $requestInfo['orderBy'] = $requestInfo['orderBy'] ?? 'accounts_payable_approval_flows.id';
-        $accountsPayableApprovalFlows = Utils::pagination($accountsPayableApprovalFlow, $requestInfo);
-
-        foreach ($accountsPayableApprovalFlows as  $accountsPayableApprovalFlow) {
-            if ($accountsPayableApprovalFlow['payment_request'] != null) {
-                foreach ($accountsPayableApprovalFlow['payment_request']['purchase_order'] as $purchaseOrder) {
-                    foreach ($purchaseOrder->purchase_order_installments as $key => $installment) {
-                        $installment = [
-                            'id' => $installment->installment_purchase->id,
-                            'amount_received' => $installment->amount_received,
-                            'purchase_order_id' => $installment->installment_purchase->purchase_order_id,
-                            'parcel_number' => $installment->installment_purchase->parcel_number,
-                            'portion_amount' => $installment->installment_purchase->portion_amount,
-                            'due_date' => $installment->installment_purchase->due_date,
-                            'note' => $installment->installment_purchase->note,
-                            'percentage_discount' => $installment->installment_purchase->percentage_discount,
-                            'money_discount' => $installment->installment_purchase->money_discount,
-                            'invoice_received' => $installment->installment_purchase->invoice_received,
-                            'invoice_paid' => $installment->installment_purchase->invoice_paid,
-                            'payment_request_id' => $installment->installment_purchase->payment_request_id,
-                            'amount_paid' => $installment->installment_purchase->amount_paid,
-                        ];
-                        $purchaseOrder->purchase_order_installments[$key] = $installment;
-                    }
-                }
-            }
-        }
-        return $accountsPayableApprovalFlows;
+        $paymentRequest = Utils::baseFilterReportsPaymentRequest($paymentRequest, $requestInfo);
+        $requestInfo['orderBy'] = $requestInfo['orderBy'] ?? 'id';
+        return Utils::pagination($paymentRequest, $requestInfo);
     }
 
     public function approveAccount($id)
