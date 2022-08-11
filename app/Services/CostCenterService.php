8b88ac23df54864c931d5351a7d41611ba0bd886
Alisson de S. Santos <alisson.souza@in8.com.br>
2022-08-11 16:46:29 -0300

    fix: solve problem nestable

--- app/Services/CostCenterService.php
+++ app/Services/CostCenterService.php
@@ -55,7 +55,7 @@ class CostCenterService
             ->where('parent', null)
             ->whereIn('id', $costCenterID)
             , $requestInfo);
-            $nestable = $this->costCenter->with($this->with)->nestable($costCenters);
+            $nestable = $this->costCenter->nestable($costCenters);
             foreach($nestable as $nest){
                 $nest->group_approval_flow = GroupApprovalFlow::where('id', $nest->group_approval_flow_id)->first();
             }
