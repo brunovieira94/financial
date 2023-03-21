a103e7f22d8887ceef44c34a107c91db960b49f3
Alisson de Souza <alisson.souza@in8.com.br>
2023-03-21 18:15:51 -0300

    CU-2xhhbuj: added downtime request in back-end - main

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -69,5 +69,6 @@ class Kernel extends HttpKernel
         'check.values.invoice' => \App\Http\Middleware\CheckValuesWhenSendingInvoice::class,
         'check.values.payment.request.integration' => \App\Http\Middleware\CheckStorePaymentRequestIntegration::class,
         'check.data.generate.cnab' => \App\Http\Middleware\CheckDataRequestGenerateCNAB::class,
+        'downtime.user' => \App\Http\Middleware\DownTimeUser::class,
     ];
 }
