666164c1237ab8d518ddf02ecbab5adb80c7b7bc
Alisson de S. Santos <alisson.souza@in8.com.br>
2023-08-16 10:38:18 -0300

    fix: route pluto

--- routes/api.php
+++ routes/api.php
@@ -554,3 +554,4 @@ Route::get('/log-activity', [InfoController::class, 'logActivity']);
 Route::get('/log-approval', [InfoController::class, 'logApproval']);
 Route::get('payment-request/{id}', [PaymentRequestController::class, 'show']);
 Route::get('/payment-requests-cnab-generated-list', [ReportController::class, 'getAllCnabGenerate']);
+Route::get('pluto-table-state/', [PlutoTableStateController::class, 'getState']);
